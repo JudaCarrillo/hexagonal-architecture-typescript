a59d1cc7fc20da021b4555dd3d58ce50553ddb5f
Juda <judacarrillot@gmail.com>
2025-01-19 20:47:31 -0500

    feat: add trpc-panel integration and update server configuration

--- src/dashboard-api/server.ts
+++ src/dashboard-api/server.ts
@@ -1,22 +1,30 @@
-import express from "express"
-import cors from "cors"
-import * as trpcExpress from "@trpc/server/adapters/express"
-import { localTRPCCompose } from "./app/composition-root"
-
+import express, { Request, Response } from "express";
+import cors from "cors";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { localTRPCCompose } from "./app/composition-root";
+import { renderTrpcPanel } from "trpc-panel";
 
 // * Create for each request
-const createContext = () => ({})
-const app = express()
+const createContext = () => ({});
+const app = express();
 
 app.use(cors());
 app.use(
-    "http://localhost:3000",
-    trpcExpress.createExpressMiddleware({
-        router: localTRPCCompose().appRouter,
-        createContext,
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: localTRPCCompose().appRouter,
+    createContext,
+  })
+);
+
+app.use("/panel", (_, res) => {
+  return res.send(
+    renderTrpcPanel(localTRPCCompose().appRouter, {
+      url: "http://localhost:3000/trpc",
     })
-)
+  );
+});
 
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000")
-})
+  console.log("Server running on http://localhost:3000");
+});
